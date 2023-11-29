021f90f5eba232bda1609844fa6aa8c90808c7f1
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-11-29 21:33:10 +0800

    OIK

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 
 const config = {
   birthdate: 'Nov 29, 2023',
-  name: 'Tiara Desmitha Oliviany'
+  name: 'TiaraDO'
 };
 
 function hideEverything() {
